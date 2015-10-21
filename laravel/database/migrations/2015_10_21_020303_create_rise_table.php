4dd49ef2139403c569cc8c4c8697a6a3cc367b57
佐竹映季 <satake@satakeutsuki-no-MacBook-Air.local>
2015-10-21 12:26:51 +0900

    [mod] migrationファイル差し替え

--- laravel/database/migrations/2015_10_21_020303_create_rise_table.php
+++ laravel/database/migrations/2015_10_21_020303_create_rise_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateRiseTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,12 +12,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
+        Schema::create('rise', function (Blueprint $table) {
+            $table->increments('rise_id');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password', 60);
-            $table->rememberToken();
+            $table->string('email');
+            $table->string('apply_date');
+            $table->string('volume');
             $table->timestamps();
         });
     }
@@ -29,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::drop('users');
+        Schema::drop('rise');
     }
 }
