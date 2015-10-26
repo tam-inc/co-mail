15a1d45ab46c1a62de3162563dc5dfc9bd727d40
佐竹映季 <satake@satakeutsuki-no-MacBook-Air.local>
2015-10-26 17:34:54 +0900

    [mod] riceサービス todayメソッドの中身をモジュール化

--- laravel/app/Service/riceService.php
+++ laravel/app/Service/riceService.php
@@ -10,10 +10,12 @@ use DB;
  * Time: 14:28
  */
 
+class RiceService
+{
 
-class RiceService{
-
-    public function createApplyTable($params){
+    //申し込み者をDBに入れる
+    public function createApplyTable($params)
+    {
 
         $input_time = \Carbon\Carbon::now()->toDateTimeString();
 
@@ -23,43 +25,124 @@ class RiceService{
             'volume' => $params['rice'],
             'winner' => 0,
             'created_at' => $input_time,
-            'updated_at' => $input_time,
+            'updated_at' => $input_time
         ]);
-
-        return;
 
     }
 
-    public function today(){
+    //現在の受付状況等をJSONで返す
+    public function today()
+    {
+        return json_encode([
+            "status" => "OK",
+            "is_open" => $this->getIsOpen(),
+            "winner" => $this->getWinner(),
+            "subscriber" => $this->getSubscriber(),
+        ]);
+    }
 
-        $today = array();
-        $today['now'] = \Carbon\Carbon::now();
-        $today['date'] = $today['now']->format('Y-m-d');
+    //受付中かどうか
+    protected function getIsOpen(){
 
-        //本日の申込者を取ってくる
-        $subscriber = DB::table('rice')
+        $today = [
+            'now' => \Carbon\Carbon::now(),
+            'y-m-d' =>  \Carbon\Carbon::now()->format('Y-m-d'),
+        ];
+
+        if ($today['now']->hour > 11) {
+            return $is_open = false;
+        } else if($today['now']->hour > 8){
+            return $is_open = true;
+        }
+
+    }
+
+    //米を炊く人を選出
+    protected function getWinner(){
+
+        $today = [
+            'now' => \Carbon\Carbon::now(),
+            'y-m-d' =>  \Carbon\Carbon::now()->format('Y-m-d'),
+            'one_week_ago' => \Carbon\Carbon::now()->subDay(7)->format('Y-m-d'),
+        ];
+
+        $sql = <<<SQL
+SELECT
+	rice.id,
+	rice.user_id,
+	users.name AS name,
+	COALESCE(count_table.count,0) as count
+FROM (rice LEFT JOIN users ON rice.user_id = users.id)
+LEFT JOIN (
+	SELECT
+		user_id,
+		count(*) as count
+	FROM rice
+	where date BETWEEN :one_week_ago AND :today
+	and winner = '1'
+	group by user_id
+) as count_table ON count_table.user_id = rice.user_id
+WHERE date=:today
+SQL;
+        $bind = [
+            "today" => $today['y-m-d'],
+            "one_week_ago" => $today['one_week_ago'],
+        ];
+        $pickup = DB::select($sql,$bind);
+
+
+        //row->countをkeyにして配列を作成する
+        $result = [];
+        foreach($pickup as $row ){
+            $tmp = array_get($result,$row->count,[]);
+            $tmp[] = $row;
+            $result[$row->count] = $tmp;
+        }
+
+        $min = min(array_keys($result));
+        $candidate = $result[$min];
+
+        //候補者からランダムに選出
+        $result_key = array_rand($candidate);
+        $winner = $candidate[$result_key];
+
+        return (array)$winner;
+
+    }
+
+    //本日の申込者を取ってくる
+    protected function getSubscriber(){
+
+        $today = [
+            'now' => \Carbon\Carbon::now(),
+            'y-m-d' =>  \Carbon\Carbon::now()->format('Y-m-d'),
+        ];
+
+        $result = DB::table('rice')
             ->leftJoin('users', 'rice.user_id', '=', 'users.id')
-            ->where('date',$today['date'])
+            ->where('date', $today['y-m-d'])
             ->select('user_id', 'name', 'volume')
             ->get();
 
-        //受付中&結果発表前かどうか
-        if($today['now']->hour > 11){
-            $is_open = false;
-            $limit = true;
-        } else if($today['now']->hour > 8){
-            $is_open = true;
-            $limit = false;
-        }
+        return $result;
 
-        return json_encode([
-            "status" => "OK",
-            "is_open" => $is_open,
-            "limit" => $limit,
-            "subscriber" => $subscriber,
-        ]);
+    }
+
+    //本日の分量を取ってくる フロントで計算？
+    protected function getTotalVolume(){
+
+        $today = [
+            'now' => \Carbon\Carbon::now(),
+            'y-m-d' =>  \Carbon\Carbon::now()->format('Y-m-d'),
+        ];
+
+        $result = DB::table('rice')
+            ->where('date', $today['y-m-d'])
+            ->where('volume', '>', 0)
+            ->sum('volume');
+
+        return $result;
 
     }
 
 }
-
