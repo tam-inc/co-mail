4069002c7bbacadd32c9bbd561e5e93b92fe7a6f
佐竹映季 <satake@satakeutsuki-no-MacBook-Air.local>
2015-10-29 15:14:43 +0900

    [mod] googleIDでuser_idを取得する記述を追加。+ インデント整理

--- laravel/app/Service/UserService.php
+++ laravel/app/Service/UserService.php
@@ -13,31 +13,45 @@ use DB;
 
 class UserService{
 
-    public function createUser($google_info){
+    public function createUser( $google_info , $google_id )
+    {
 
-            DB::table('users')->insert([
-                'google_id' => $google_info->getId(),
-                'name' => $google_info->getName(),
-                'email' => $google_info->getEmail(),
-                'created_at' => \Carbon\Carbon::now()->toDateTimeString(),
-                'updated_at' => \Carbon\Carbon::now()->toDateTimeString(),
-            ]);
+        $now = \Carbon\Carbon::now();
 
-            $user = DB::table('users')->where('google_id',$google_id)->first();
+        DB::table( 'users' )->insert([
+
+            'google_id'  => $google_id,
+            'name'       => $google_info->getName(),
+            'email'      => $google_info->getEmail(),
+            'created_at' => $now->toDateTimeString(),
+            'updated_at' => $now->toDateTimeString(),
+
+        ]);
+
+    }
+
+    public function getUser( $id )
+    {
+
+        $user = DB::table( 'users' )
+            ->where( 'id' , $id )
+            ->select( 'id' , 'name' , 'email' )
+            ->first();
 
         return $user;
 
     }
 
-    public function getUser($google_id)
+    public function getUserByGoogleID( $google_id )
     {
 
-        $user = DB::table('users')
-            ->where('google_id', $google_id)
-            ->select('id', 'name', 'email')
+        $user = DB::table( 'users' )
+            ->where( 'google_id' , $google_id )
+            ->select( 'id' )
             ->first();
 
         return $user;
+
     }
 
 }
