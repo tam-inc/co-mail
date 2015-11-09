fc6429187c9d75842d2ede4c1ef623091933b8fc
佐竹映季 <satake@satakeutsuki-no-MacBook-Air.local>
2015-11-09 22:56:56 +0900

    [mod] php artisanコマンド selectWinner定義

--- laravel/app/Console/Commands/SelectWinner.php
+++ laravel/app/Console/Commands/SelectWinner.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use App\Service\RiceService;
 use DB;
 
 class SelectWinner extends Command
@@ -19,7 +20,7 @@ class SelectWinner extends Command
      *
      * @var string
      */
-    protected $description = 'decide todays winner' ;
+    protected $description = 'insert todays winner' ;
 
     /**
      * Execute the console command.
@@ -27,57 +28,10 @@ class SelectWinner extends Command
      * @return mixed
      */
 
-    public function fire(){
+    public function handle( RiceService $riceService )
+    {
 
-        $today = [
-            'now' => \Carbon\Carbon::now(),
-            'y-m-d' =>  \Carbon\Carbon::now()->format('Y-m-d'),
-            'one_week_ago' => \Carbon\Carbon::now()->subDay(7)->format('Y-m-d'),
-        ];
-
-
-
-        $sql = <<<SQL
-SELECT
-	rice.id,
-	rice.user_id,
-	users.name AS name,
-	COALESCE(count_table.count,0) as count
-FROM (rice LEFT JOIN users ON rice.user_id = users.id)
-LEFT JOIN (
-	SELECT
-		user_id,
-		count(*) as count
-	FROM rice
-	where date BETWEEN :one_week_ago AND :today
-	and winner = '1'
-	group by user_id
-) as count_table ON count_table.user_id = rice.user_id
-WHERE date=:today
-SQL;
-        $bind = [
-            "today" => $today['y-m-d'],
-            "one_week_ago" => $today['one_week_ago'],
-        ];
-        $pickup = DB::select($sql,$bind);
-
-
-        //row->countをkeyにして配列を作成する
-        $result = [];
-        foreach($pickup as $row ){
-            $tmp = array_get($result,$row->count,[]);
-            $tmp[] = $row;
-            $result[$row->count] = $tmp;
-        }
-
-        $min = min(array_keys($result));
-        $candidate = $result[$min];
-
-        //候補者からランダムに選出
-        $result_key = array_rand($candidate);
-        $winner = $candidate[$result_key];
-
-        var_dump($winner);
+        $riceService->selectWinner();
 
     }
 }
