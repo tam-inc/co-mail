f01f063bb08a1f858d758298d199dc305d36e535
Kusamao Abe <kusamao@tam-tam.co.jp>
2015-11-11 20:54:14 +0900

    [mod] hbsHelperで、小数点表記の数字のStringを渡されると比較に失敗するので型キャストを別で

--- helpers/equal.js
+++ helpers/equal.js
@@ -4,7 +4,7 @@
 var _ = require( 'underscore' );
 
 module.exports = function ( value, comparison, block ) {
-	if ( _.isNumber( value ) ) value = String( value );
+	if ( _.isNumber( value ) ) comparison = Number( comparison );
 	if ( value === comparison ) return block.fn( this );
 	return block.inverse( this );
 };
