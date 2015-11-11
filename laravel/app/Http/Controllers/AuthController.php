de34ac4cdb19f339e046e189561b50a13216b699
佐竹映季 <satake@satakeutsuki-no-MacBook-Air.local>
2015-11-11 23:23:46 +0900

    [mod] 認証系コントローラ、モデル、行の整理など

--- laravel/app/Http/Controllers/AuthController.php
+++ laravel/app/Http/Controllers/AuthController.php
@@ -8,66 +8,57 @@ use Session;
 
 class AuthController extends Controller
 {
-
     use RestControllerTrait;
 
     //ログインチェック
-    public function getUser( UserService $userService ){
+    public function getUser(UserService $userService)
+    {
+        //セッションからユーザーIDを取得
+        $session = Session::get('auth');
 
-        $session = Session::get( 'auth' );
-
-        if( $session ){
-
-            $user = $userService -> getUser( $session );
-
-            return $this->responseOk([
-
-                "user" => $user,
-
-            ]);
-
-        } else {
-
+        //セッションが無かったらエラーを返す
+        if (!$session) {
             return $this->responseBad([
-
                 "message" => "ログインしていません。",
-
             ]);
-
         }
 
+        //セッションからユーザー情報を取得
+        $user = $userService->getUser($session);
+
+        return $this->responseOk([
+            "user" => $user,
+        ]);
     }
+
 
     //google認証
     public function auth()
     {
-
-        return Socialite::with( 'google' )->redirect();
-
+        return Socialite::with('google')->redirect();
     }
+
 
     //google認証リダイレクトページ
-    public function googleCallback( UserService $userService )
+    public function googleCallback(UserService $userService)
     {
-
-        $google_info = Socialite::with( 'google' )->user();
-
+        $google_info = Socialite::with('google')->user();
         $google_id = $google_info->getId();
 
-        $user = $userService -> getUserByGoogleID( $google_id );
+        //googleのIDからユーザー情報を取得
+        $user = $userService->getUserByGoogleID($google_id);
 
-        if( !$user ) {
-
-            $userService -> createUser( $google_info , $google_id );
-
-            $user = $userService -> getUserByGoogleID( $google_id );
-
+        if (!$user) {
+            //ユーザー情報を作成する
+            $userService->createUser($google_info, $google_id);
+            //ユーザーテーブルのユーザーIDをセッションに入れるため、ユーザー情報をユーザーテーブルから取得する必要がある。
+            $user = $userService->getUserByGoogleID($google_id);
         }
 
-        Session::put( 'auth' , $user->id );
+        //セッションにuserのIDを格納
+        Session::put('auth', $user->id);
 
-        return redirect( '/' );
-
+        //todo area分け
+        return redirect('/tokyo');
     }
-
 }
