5aace03e4b3f182bc9a6f0be6a0c38e0ce165b18
Kusamao Abe <kusamao@tam-tam.co.jp>
2015-11-16 11:57:03 +0900

    [add] result, sleep Viewのファイル追加

--- scripts/router/router.js
+++ scripts/router/router.js
@@ -7,7 +7,13 @@ module.exports = Backbone.Router.extend( {
 
 	ApplyView:   require( '../views/apply.js' ),
 	ConfirmView: require( '../views/confirm.js' ),
+	ResultView:  require( '../views/result.js' ),
+	SleepView:   require( '../views/sleep.js' ),
 
+	/**
+	 * コンストラクタ
+	 * @param options {Object} Instance化される時に渡される
+	 */
 	initialize: function ( options ) {
 		var self       = this;
 		var TodayModel = require( '../models/rice/today.js' );
@@ -23,6 +29,8 @@ module.exports = Backbone.Router.extend( {
 	changeView: function ( model, viewName ) {
 		var self = this;
 		Backbone.history.navigate( '/' + viewName );
+
+		// viewNameが渡ってきたらその名前をキャメルケースに変換してViewをつけてInstance化する
 		var viewNameKey = viewName.charAt(0).toUpperCase() + viewName.slice(1) + 'View',
 			View = this[ viewNameKey ],
 			view = new View( { model: self.todayModel } );
