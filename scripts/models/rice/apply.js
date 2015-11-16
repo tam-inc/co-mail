36ad176dcf02130970c5a1749df94abf78419cc3
Kusamao Abe <kusamao@tam-tam.co.jp>
2015-11-16 11:57:41 +0900

    [mod] formViewでvalidateメソッドを使ってみる

--- scripts/models/rice/apply.js
+++ scripts/models/rice/apply.js
@@ -3,5 +3,8 @@
  */
 
 module.exports = Backbone.Model.extend( {
-	url: 'http://tambourine.herokuapp.com/v1/rice/apply'
+	url: 'http://tambourine.herokuapp.com/v1/rice/apply',
+	validate: function ( data ) {
+		if ( data.rice < 0 || data.rice >= 2 ) return 'error';
+	}
 } );
--- scripts/views/confirm/form.js
+++ scripts/views/confirm/form.js
@@ -5,7 +5,13 @@
 module.exports = Backbone.View.extend( {
 	el: '.confirm-form',
 	template: require( '../../../templates/confirm/form.handlebars' ),
+	events: {
+		'change .rice_quantity': 'onChange',
+		'click button':          'onSubmit'
+	},
 	initialize: function () {
+		this.listenTo( this.model, 'invalid', this.onError );
+		this.rice = this.model.get( 'rice' );
 	},
 	render: function () {
 		var self         = this,
@@ -13,5 +19,14 @@ module.exports = Backbone.View.extend( {
 			viewTemplate = template( self.model.toJSON() );
 		self.$el.append( viewTemplate );
 		return self;
+	},
+	onSubmit: function () {
+		this.model.set( { rice: this.rice }, { validate: true } );
+	},
+	onChange: function () {
+		this.rice = Number( this.$( '.rice_quantity' ).val() );
+	},
+	onError: function ( args ) {
+		console.error( args );
 	}
 } );
